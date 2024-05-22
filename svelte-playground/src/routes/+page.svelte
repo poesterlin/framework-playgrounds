0b9446ea3ad208372e2df6044edd0c0c4832aaf5
Philip Oesterlin <philip.oesterlin@gmail.com>
2024-05-22 15:54:30 +0200

    update svelte example

--- svelte-playground/src/routes/+page.svelte
+++ svelte-playground/src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { browser } from '$app/environment';
 	import { onDestroy, onMount } from 'svelte';
 
 	let spinnerPosition = 0;
@@ -33,13 +34,47 @@
 	<h1>Function View</h1>
 	<p>This view uses a function to run an expensive operation.</p>
 	<p>Current spinner position: {spinnerPosition}</p>
-	<p>
-		Result of expensive operation: {new Array(3000000)
-			.fill(0)
-			.map((_, i) => Math.sqrt(i))
-			.map((n) => n * n)
-			.map(String)
-			.reduce(() => 'done')}
-	</p>
+	<div class="spinner" style:--spinner-position="{spinnerPosition}%"></div>
+
+	{#if browser}
+		<p>
+			Result of expensive operation
+			{`
+			{{
+				new Array(3000000)
+					.fill(0)
+					.map((_, i) => Math.sqrt(i))
+					.map((n) => n * n)
+					.map(String)
+					.reduce(() => 'done')
+			}}
+		`}
+
+			{new Array(3000000)
+				.fill(0)
+				.map((_, i) => Math.sqrt(i))
+				.map((n) => n * n)
+				.map(String)
+				.reduce(() => 'done')}
+		</p>
+	{/if}
 	<button on:click={expensiveToRunFunction}>Run Expensive Function </button>
 </main>
+
+<style>
+	.spinner {
+		position: relative;
+		width: 100%;
+		height: 100px;
+	}
+
+	.spinner::after {
+		content: '';
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		left: var(--spinner-position);
+		width: 30px;
+		background-color: #333;
+	}
+</style>
